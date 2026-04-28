b338ab5f83fae3b0b50a5c67c5e9d447f9350c96
Gennady Oleshko <gennady@oleshko-g.ru>
2026-04-28 20:52:13 +0300

    add tests: Authorize, Connect

--- internal/service/depositor/service_test.go
+++ internal/service/depositor/service_test.go
@@ -76,3 +76,55 @@ func TestRegister(t *testing.T) {
 		})
 	})
 }
+
+func TestAuthorize(t *testing.T) {
+	svc := depositor.New(&storage.DepositorMock{})
+
+	t.Run("Success", func(t *testing.T) {
+		_, err := svc.Authorize(
+			nil,
+			&pb.AuthorizeRequest{},
+		)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		t.Run("Empty Request", func(t *testing.T) {
+			_, err := svc.Authorize(
+				nil,
+				nil,
+			)
+			if err == nil {
+				t.Errorf("expected error, got nil")
+			}
+		})
+	})
+}
+
+func TestConnect(t *testing.T) {
+	svc := depositor.New(&storage.DepositorMock{})
+
+	t.Run("Success", func(t *testing.T) {
+		_, err := svc.Connect(
+			nil,
+			&pb.ConnectRequest{},
+		)
+		if err != nil {
+			t.Error(err)
+		}
+	})
+
+	t.Run("Err", func(t *testing.T) {
+		t.Run("Empty Request", func(t *testing.T) {
+			_, err := svc.Connect(
+				nil,
+				nil,
+			)
+			if err == nil {
+				t.Errorf("expected error, got nil")
+			}
+		})
+	})
+}
