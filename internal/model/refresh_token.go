8689266db08286d87b5d2ddfc557d0afc1392285
Gennady Oleshko <gennady@oleshko-g.ru>
2026-04-29 22:02:30 +0300

    Update refresh_token.go

--- internal/model/refresh_token.go
+++ internal/model/refresh_token.go
@@ -1,34 +1,51 @@
+// Package model contains the data models used by the gophkeeper application.
 package model
 
 import "time"
 
 // RefreshToken is the model of a refresh token
 // It enforces the relationship between a public key and a refresh token
-type RefreshToken struct {
+type RefreshToken[T RefreshTokenValue] struct {
 	// PubKeyID is the ID of the public key associated with the refresh token
 	PubKeyID string
 	// ID is the actual refresh token value
-	ID        RefreshTokenValue
-	TTL       time.Duration
+	ID T
+	// TTL is the time-to-live of the refresh token
+	TTL time.Duration
+	// RevokedAt is the timestamp at which the refresh token has been revoked
 	RevokedAt time.Time
 }
 
+// RefreshTokenValue is the interface that a refresh token value must implement.
+// It's intended to be:
+//   - instantiated with [uuid.UUID].
+//   - populated with a UUID v7 value. So the ID iself could answer at what timestamp the refresh token has been issued.
 type RefreshTokenValue interface {
 	Time() time.Time
 }
 
-func (r *RefreshToken) IsExpired() bool {
-	expiresAt := r.ID.Time().Add(r.TTL)
-	if time.Now().After(expiresAt) {
+// IssuedAt returns the timestamp at which the refresh token has been issued.
+func (r *RefreshToken[T]) IssuedAt() time.Time {
+	return r.ID.Time()
+}
+
+// ExpiresAt returns the timestamp at which the refresh token expires.
+func (r *RefreshToken[T]) ExpiresAt() time.Time {
+	return r.IssuedAt().Add(r.TTL)
+}
+
+// IsExpired returns true if the refresh token has expired.
+func (r *RefreshToken[T]) IsExpired() bool {
+	if time.Now().After(r.ExpiresAt()) {
 		return true
 	}
 
 	return false
 }
 
-func (r *RefreshToken) IsRevoked() bool {
-	issuedAt := r.ID.Time()
-	if issuedAt.After(r.RevokedAt) {
+// IsRevoked returns true if the refresh token has been revoked.
+func (r *RefreshToken[T]) IsRevoked() bool {
+	if r.IssuedAt().After(r.RevokedAt) {
 		return true
 	}
 	return false
