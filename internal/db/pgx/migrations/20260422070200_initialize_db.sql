e9d6c41a1771ff65d73779948cd56c9f2c0cbca0
Gennady Oleshko <gennady@oleshko-g.ru>
2026-04-29 21:47:06 +0300

    refactor the db schema

--- internal/db/pgx/migrations/20260422070200_initialize_db.sql
+++ internal/db/pgx/migrations/20260422070200_initialize_db.sql
@@ -1,36 +1,42 @@
 -- +goose Up
-CREATE TABLE depositor_pub_keys (id UUID PRIMARY KEY, pub_key TEXT NOT NULL);
+CREATE TABLE depositor_pub_keys (id UUID PRIMARY KEY, pub_key TEXT UNIQUE NOT NULL);
 
 
 CREATE TABLE depositor_refresh_tokens (
-  id UUID PRIMARY KEY,
-  token TEXT NOT NULL,
-  depositor_pub_key_id UUID NOT NULL,
-  issued_at TIMESTAMP WITH TIME ZONE NOT NULL,
+  token UUID PRIMARY KEY,
+  depositor_pub_key_id UUID NOT NULL REFERENCES depositor_pub_keys (id),
   revoked_at TIMESTAMP WITH TIME ZONE
 );
+
+
+COMMENT ON TABLE depositor_refresh_tokens IS 'Refresh tokens for depositors';
+
+
+COMMENT ON COLUMN depositor_refresh_tokens.token IS 'token is a UUID v7 value. It includes the timestamp at which the token was issued';
 
 
 CREATE TABLE depositor_apps (
   id UUID PRIMARY KEY,
   app_name TEXT NOT NULL,
-  depositor_pub_key_id UUID NOT NULL
+  depositor_pub_key_id UUID NOT NULL,
+  CONSTRAINT app_name_depositor_pub_key_id UNIQUE (app_name, depositor_pub_key_id)
 );
 
 
 CREATE TABLE deposited_data (
   id UUID PRIMARY KEY,
-  depositor_pub_key_id UUID NOT NULL,
-  ciphered_data JSONB NOT NULL
+  depositor_pub_key_id UUID NOT NULL REFERENCES depositor_pub_keys (id) ON DELETE CASCADE,
+  encrypted_data JSONB NOT NULL
 );
 
 
 CREATE TABLE depositor_app_sessions (
   id UUID PRIMARY KEY,
-  depositor_app_id UUID NOT NULL,
-  started_at TIMESTAMP WITH TIME ZONE NOT NULL,
-  expired_at TIMESTAMP WITH TIME ZONE
+  depositor_app_id UUID NOT NULL REFERENCES depositor_apps (id) ON DELETE CASCADE
 );
+
+
+COMMENT ON COLUMN depositor_app_sessions.id IS 'token is a UUID v7 value. It includes the timestamp at which was started';
 
 
 -- +goose Down
