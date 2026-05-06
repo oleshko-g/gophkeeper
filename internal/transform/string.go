5a9dd5e78f7bfe119f90006795305f39c5b8697f
Gennady Oleshko <gennady@oleshko-g.ru>
2026-05-06 09:05:29 +0300

    Update string.go

--- internal/transform/string.go
+++ internal/transform/string.go
@@ -8,15 +8,15 @@ import (
 
 // StringFromReader returns a single use iterator over an [io.Reader].
 // If the reader can be read into memory, it yields the contents as a single string.
-// If the reader cannot be read into memory, it yields an empty string.
+// If the iterator encounters an error, it yields the contents read so far.
 func StringFromReader(r io.Reader) iter.Seq[string] {
-	var b bytes.Buffer
 	return func(yield func(string) bool) {
-		_, err := b.ReadFrom(r)
+		b := bytes.Buffer{}
+		n, err := b.ReadFrom(r)
 		if err == nil {
 			yield(b.String())
 			return
 		}
-		yield("")
+		yield(b.String()[:n])
 	}
 }
