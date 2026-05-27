09a1c64e8c96003c29967df7f336261e0c865e98
Gennady Oleshko <gennady@oleshko-g.ru>
2026-05-27 20:37:15 +0300

    Update retrieve_secrets.go

--- internal/service/keeper/retrieve_secrets.go
+++ internal/service/keeper/retrieve_secrets.go
@@ -5,19 +5,21 @@ import (
 
 	pb "github.com/oleshko-g/gophkeeper/api/v1"
 	"github.com/oleshko-g/gophkeeper/internal/service"
+	uuidv7 "github.com/oleshko-g/gophkeeper/internal/uuid-v7"
 )
 
 func (s *Service) RetrieveSecret(ctx context.Context, in *pb.RetrieveSecretRequest) (*pb.RetrieveSecretResponse, error) {
-	// Authenticate the user
 	publicKeyID, ok := service.PubKeyIDFromCtx(ctx)
 	if !ok {
 		return nil, service.WrapError(&service.Err{Type: service.ErrTypeUnauthenticated, SvcName: s.Name()}, errEmptyPubKeyID)
 	}
 
-	// RetrieveSecret from storage
-	// s.Keeper.
+	secretData, err := s.Keeper.RetrieveSecret(ctx, uuidv7.FromString(publicKeyID))
+	if err != nil {
+		return nil, service.WrapError(
+			&service.Err{Type: service.ErrTypeStorage, SvcName: s.Name()},
+			err)
+	}
 
-	// return response
-	_ = publicKeyID
-	return &pb.RetrieveSecretResponse{}, nil
+	return &pb.RetrieveSecretResponse{Payload: secretData}, nil
 }
