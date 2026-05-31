1fc367bbe5898e80fdb267c3ab761066cbf1c3c7
Gennady Oleshko <gennady@oleshko-g.ru>
2026-05-31 18:47:42 +0300

    add client.delete

---
+++ client/delete.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"context"
+	"io"
+	"os"
+	_ "os"
+	"time"
+
+	pb "github.com/oleshko-g/gophkeeper/api/v1"
+	_ "github.com/oleshko-g/gophkeeper/internal/uuid-v7"
+	"github.com/spf13/cobra"
+)
+
+func (app *a) deleteRunE(cmd *cobra.Command, _ []string) error {
+	var in io.Reader = cmd.InOrStdin()
+
+	deadline, ok := cmd.Context().Deadline()
+	if !ok {
+		timeout := 1 * time.Second
+		deadline = time.Now().Add(timeout)
+	}
+
+	if v, ok := in.(*os.File); ok {
+		if err := v.SetReadDeadline(deadline); err != nil {
+			return err
+		}
+	}
+
+	data, err := io.ReadAll(in)
+	if err != nil {
+		return err
+	}
+
+	if len(data) == 0 {
+		return errEmptyInput
+	}
+
+	ctx, cancel := context.WithTimeout(cmd.Context(), 150*time.Millisecond)
+	defer cancel()
+
+	secretID, err := depositedSecretID(data)
+	if err != nil {
+		return err
+	}
+
+	app.client.PurgeSecret(ctx, &pb.PurgeSecretRequest{SecretId: secretID})
+
+	return nil
+}
---
+++ client/errors.go
@@ -0,0 +1,5 @@
+package main
+
+import "errors"
+
+var errEmptyInput = errors.New("empty input")
